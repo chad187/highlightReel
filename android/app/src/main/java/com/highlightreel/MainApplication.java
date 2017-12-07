b91a6a1b6ed53abd60e78177717781a0808c04df
Chad C <chad187@gmail.com>
2017-12-07 14:21:51 -0800

    works with fabric but sometimes failes to run

--- android/app/src/main/java/com/highlightreel/MainApplication.java
+++ android/app/src/main/java/com/highlightreel/MainApplication.java
@@ -2,21 +2,24 @@ package com.highlightreel;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
-import com.fileopener.FileOpenerPackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
-import com.github.yamill.orientation.OrientationPackage;
-import com.rnfs.RNFSPackage;
-import com.shahenlibrary.RNVideoProcessingPackage;
-import com.reactlibrary.RNVideoEditorPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.crashlytics.android.Crashlytics;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.fileopener.FileOpenerPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.reactlibrary.RNVideoEditorPackage;
+import com.rnfs.RNFSPackage;
+import com.shahenlibrary.RNVideoProcessingPackage;
 
 import java.util.Arrays;
 import java.util.List;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -54,6 +57,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
